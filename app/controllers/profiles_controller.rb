37fcb3b49d66d202adc77093f4b9e0a528de1bd4
Manny Kandola <manny@mannykandola.com>
2016-02-16 03:26:53 +0000

    Added users show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,16 @@
 class ProfilesController < ApplicationController
    def new
     # form where a user can fill out their own profile
-    @user = User.find(params[:user_id])
+    @user = User.find( params[:user_id] )
     @profile = @user.build_profile
    end
    
    def create 
-       @user = User.find(params[:user_id])
+       @user = User.find( params[:user_id] )
        @profile = @user.build_profile(profile_params)
         if @profile.save
             flash[:success] = "Profile Updated!"
-            redirect_to user_path(params[:user_id])
+            redirect_to user_path( params[:user_id] )
         else
             render action: :new
         end
